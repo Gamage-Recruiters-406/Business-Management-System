0f1577da230489e7ce6ea55df9f6f1ce0f41a7b6
Piyumal <pasindupiumalcrw2002@gmail.com>
2026-03-04 17:35:04 +0530

    update routes

--- backend/routes/leadRoutes.js
+++ backend/routes/leadRoutes.js
@@ -12,10 +12,16 @@ import { requiredSignIn, isAdmin } from "../middlewares/AuthMiddleware.js";
 const router = express.Router();
 
 // Admin-only Lead Management
+
+//create
 router.post("/create", requiredSignIn, isAdmin, createLead);
+//all leads
 router.get("/all", requiredSignIn, isAdmin, getLeads);
+//get single lead
 router.get("/single/:id", requiredSignIn, isAdmin, getSingleLead);
+//update
 router.put("/update/:id", requiredSignIn, isAdmin, updateLead);
+//delete
 router.delete("/delete/:id", requiredSignIn, isAdmin, deleteLead);
 
 export default router;
