84bbf3c21a03d786c3ef2b520e1c6965750ad0b0
Lihini Apsara <apsaralihini11@gmail.com>
2026-03-05 14:43:47 +0530

    Employee Page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EmployeeManagement from './pages/EmployeeManagement'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <EmployeeManagement />
+    </>
+  )
+}
+
+export default App
