2e977c763c3cb012e9bf2ba066e1b6f14833583c
Dulshan Sachinthaka <162723155+Alphaiii3@users.noreply.github.com>
2026-03-09 15:28:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< Updated upstream
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-=======
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
->>>>>>> Stashed changes
 import Header from "./components/Header"; 
 import Home from "./pages/Home";
 import EmployeeManagement from "./pages/EmployeeManagement";
@@ -15,31 +11,10 @@ import SignInPage from "./pages/login/SignInPage";
 import SignUpPage from "./pages/login/SignUpPage";
 import Reports from "./pages/admin_Reports/ReportsDashboard";
 
-<<<<<<< Updated upstream
-function App() {
-  return (
-    <BrowserRouter>
-      {/* Your finished Header is now integrated */}
-      <Header />
-
-      <main className="min-h-screen bg-gray-50">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/employees" element={<EmployeeManagement />} />
-          <Route path="/tasks" element={<TaskManagement />} />
-          <Route path="/leads" element={<LeadManagement />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/reports" element={<Reports />} />
-        </Routes>
-      </main>
-=======
 function AppContent() {
   const location = useLocation();
 
   const hideHeaderPaths = ["/signin", "/signup"];
-
   const shouldShowHeader = !hideHeaderPaths.includes(location.pathname);
 
   return (
@@ -66,7 +41,6 @@ function App() {
   return (
     <BrowserRouter>
       <AppContent />
->>>>>>> Stashed changes
     </BrowserRouter>
   );
 }
